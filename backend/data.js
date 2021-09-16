ba28f0674c34cbe37ca956d69c36f14819053ba3
irene macapinlac <irenekaymac@gmail.com>
2021-09-16 23:37:54 +1000

    added database onto node backend

---
+++ backend/data.js
@@ -0,0 +1,83 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Rug',
+      category: 'Rug',
+      image:'./images/p-1.png',
+      price: 1200,
+      countInStock: 4,
+      brand: 'v',
+      rating: 4.5,
+      numReviews: 3,
+      description: 'colourful'
+    },
+
+    {
+      _id: '2',
+      name: 'Rug',
+      category: 'Rug',
+      image:'./images/p-1.png',
+      price: 1200,
+      countInStock: 3,
+      brand: 'Amelia',
+      rating: 4.5,
+      numReviews: 3,
+      description: 'colourful'
+    },
+
+    {
+      _id: '3',
+      name: 'Rug',
+      category: 'Rug',
+      image:'./images/p-1.png',
+      price: 1200,
+      countInStock: 6,
+      brand: 'Amelia',
+      rating: 4.5,
+      numReviews: 3,
+      description: 'colourful'
+    },
+
+    {
+      _id: '4',
+      name: 'Rug',
+      category: 'Rug',
+      image:'./images/p-1.png',
+      price: 1200,
+      countInStock: 1,
+      brand: 'Amelia',
+      rating: 4.5,
+      numReviews: 3,
+      description: 'colourful'
+    },
+
+    {
+      _id: '5',
+      name: 'Rug',
+      category: 'Rug',
+      image:'./images/p-1.png',
+      price: 1200,
+      countInStock: 2,
+      brand: 'Amelia',
+      rating: 4.5,
+      numReviews: 3,
+      description: 'colourful'
+    },
+
+    {
+      _id: '6',
+      name: 'Rug',
+      category: 'Rug',
+      image:'./images/p-1.png',
+      price: 1200,
+      countInStock: 0,
+      brand: 'Amelia',
+      rating: 4.5,
+      numReviews: 3,
+      description: 'colourful'
+    },
+  ],
+};
+
+export default data;
