4efbfdc08aa2dc5afeb8ff5bdfa4f4830ffed7d1
irene macapinlac <irenekaymac@gmail.com>
2021-09-23 21:54:53 +1000

    adding redux to product screen

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -4,7 +4,10 @@ const {
   PRODUCT_LIST_FAIL,
 } = require('../constants/productConstants');
 
-export const productListReducer = (state = { products: [] }, aciton) => {
+export const productListReducer = (
+  state = { loading: true, products: [] },
+  aciton
+) => {
   switch(action.type){
     case PRODUCT_LIST_REQUEST:
       return {loading: true};
@@ -12,6 +15,22 @@ export const productListReducer = (state = { products: [] }, aciton) => {
       return { loading: false, products: action.payload };
       case PRODUCT_LIST_FAIL:
       return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productDetailsReducer = (
+  state = { product:{}, loading: true},
+  action
+) => {
+  switch (action.type){
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true };
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload };
+      case PRODUCT_DETAILS_FAIL:
+      return {loading: false, error: action.payload };
     default:
       return state;
   }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducers';
+import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 
 const initialState = {};
 const reducer = combineReducers({
-  productLists: productListReducer,
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
